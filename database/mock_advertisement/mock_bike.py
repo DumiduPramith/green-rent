b4c6b7a15ecd4d5c0639dbabd2f09af25158bdb3
DumiduPramith <dumidu42@gmail.com>
2023-05-02 22:27:44 +0530

    mock bike data file

---
+++ database/mock_advertisement/mock_bike.py
@@ -0,0 +1,155 @@
+BIKE = [
+    {
+        "vehicle": {
+            "type": "bike",
+            "transmissionMethod": "auto",
+            "engineCapacity": "250cc",
+            "brandId": 1
+        },
+        "advertisement": {
+            "title": "Yamaha R15 for Rent",
+            "rate": 1500,
+            "rateDuration": "daily",
+            "description": "Brand new Yamaha R15 bike for rent. Suitable for short trips and city commuting. Well-maintained and serviced regularly.\nInsurance and helmet included. Contact for more details.",
+            "phone": 712345678,
+            "districtId": 5
+        },
+        "insurance": {
+            "insuranceType": "full",
+            "collisonCoverage": True,
+            "bodyCoverage": True,
+            "medicalCoverage": False
+        }
+    },
+    {
+        "vehicle": {
+            "type": "bike",
+            "transmissionMethod": "manual",
+            "engineCapacity": "150cc",
+            "brandId": 4
+        },
+        "advertisement": {
+            "title": "TVS Apache for Sale",
+            "rate": 200000,
+            "rateDuration": "monthly",
+            "description": "TVS Apache bike for sale. Good condition and runs smoothly. Recently serviced and all documents up to date. Suitable for both city and long-distance travel.\nNegotiable price. Contact for more details.",
+            "phone": 773456789,
+            "districtId": 12
+        },
+        "insurance": {
+            "insuranceType": "third",
+            "collisonCoverage": False,
+            "bodyCoverage": True,
+            "medicalCoverage": True
+        }
+    },
+    {
+        "vehicle": {
+            "type": "bike",
+            "transmissionMethod": "auto",
+            "engineCapacity": "650cc",
+            "brandId": 10
+        },
+        "advertisement": {
+            "title": "Royal Enfield Interceptor for Rent",
+            "rate": 5000,
+            "rateDuration": "weekly",
+            "description": "Royal Enfield Interceptor bike for rent. Classic and stylish look, perfect for long rides and road trips. Well-maintained and regularly serviced.\nInsurance and helmet included. Contact for more details.",
+            "phone": 787654321,
+            "districtId": 8
+        },
+        "insurance": {
+            "insuranceType": "full",
+            "collisonCoverage": True,
+            "bodyCoverage": True,
+            "medicalCoverage": True
+        }
+    }, {
+        "vehicle": {
+            "type": "bike",
+            "transmissionMethod": "manual",
+            "engineCapacity": "150cc",
+            "brandId": 2
+        },
+        "advertisement": {
+            "title": "Honda CB Hornet for Rent",
+            "rate": 1200,
+            "rateDuration": "daily",
+            "description": "Honda CB Hornet bike for rent. Perfect for city commuting and short trips. Good condition and regularly serviced.\nInsurance and helmet included. Contact for more details.",
+            "phone": 765432109,
+            "districtId": 6
+        },
+        "insurance": {
+            "insuranceType": "third",
+            "collisonCoverage": False,
+            "bodyCoverage": True,
+            "medicalCoverage": False
+        }
+    },
+    {
+        "vehicle": {
+            "type": "bike",
+            "transmissionMethod": "manual",
+            "engineCapacity": "200cc",
+            "brandId": 3
+        },
+        "advertisement": {
+            "title": "Suzuki Gixxer SF for Sale",
+            "rate": 350000,
+            "rateDuration": "monthly",
+            "description": "Suzuki Gixxer SF bike for sale. Good condition and runs smoothly. Recently serviced and all documents up to date. Perfect for both city and highway rides.\nNegotiable price. Contact for more details.",
+            "phone": 718293456,
+            "districtId": 10
+        },
+        "insurance": {
+            "insuranceType": "full",
+            "collisonCoverage": True,
+            "bodyCoverage": True,
+            "medicalCoverage": False
+        }
+    },
+    {
+        "vehicle": {
+            "type": "bike",
+            "transmissionMethod": "manual",
+            "engineCapacity": "150cc",
+            "brandId": 5
+        },
+        "advertisement": {
+            "title": "Bajaj Pulsar 150 for Rent",
+            "rate": 1000,
+            "rateDuration": "daily",
+            "description": "Bajaj Pulsar 150 bike for rent. Good condition and runs smoothly. Suitable for short trips and city commuting.\nInsurance and helmet included. Contact for more details.",
+            "phone": 726384519,
+            "districtId": 16
+        },
+        "insurance": {
+            "insuranceType": "third",
+            "collisonCoverage": False,
+            "bodyCoverage": False,
+            "medicalCoverage": True
+        }
+    },
+    {
+        "vehicle": {
+            "type": "bike",
+            "transmissionMethod": "manual",
+            "engineCapacity": "300cc",
+            "brandId": 6
+        },
+        "advertisement": {
+            "title": "Hero Xtreme 200S for Sale",
+            "rate": 450000,
+            "rateDuration": "monthly",
+            "description": "Hero Xtreme 200S bike for sale. Excellent condition and runs smoothly. Well-maintained and serviced regularly. Suitable for both city and long-distance travel.\nNegotiable price. Contact for more details.",
+            "phone": 792837465,
+            "districtId": 13
+        },
+        "insurance": {
+            "insuranceType": "full",
+            "collisonCoverage": True,
+            "bodyCoverage": True,
+            "medicalCoverage": True
+        }
+    }
+]
