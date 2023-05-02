a0e3b2ef350c06b135fa838e2d7c2bc1c445e14b
DumiduPramith <dumidu42@gmail.com>
2023-05-02 22:27:51 +0530

    mock car data file

---
+++ database/mock_advertisement/mock_car.py
@@ -0,0 +1,215 @@
+CAR = [
+    {
+        "vehicle": {
+            "type": "car",
+            "transmissionMethod": "auto",
+            "withDriver": True,
+            "withAc": True,
+            "noOfPassengers": 5,
+            "brandId": 1
+        },
+        "advertisement": {
+            "title": "Tesla Model S for Rent",
+            "rate": 2500,
+            "rateDuration": "daily",
+            "description": "Experience the future with our Tesla Model S rental!\nLuxury electric vehicle that is perfect for short trips around the city.\nIncludes driver and air conditioning. Book now and enjoy the ride!",
+            "phone": 712345678,
+            "districtId": 5
+        },
+        "insurance": {
+            "insuranceType": "full",
+            "collisonCoverage": True,
+            "bodyCoverage": True,
+            "medicalCoverage": True
+        }
+    },
+    {
+        "vehicle": {
+            "type": "car",
+            "transmissionMethod": "manual",
+            "withDriver": False,
+            "withAc": False,
+            "noOfPassengers": 4,
+            "brandId": 2
+        },
+        "advertisement": {
+            "title": "Mazda 3 for Rent",
+            "rate": 2000,
+            "rateDuration": "weekly",
+            "description": "Get the most out of your journey with our Mazda 3 rental!\nSporty and stylish car that is perfect for a weekend getaway.\nAir conditioning available. Book now and enjoy the ride!",
+            "phone": 723456789,
+            "districtId": 11
+        },
+        "insurance": {
+            "insuranceType": "third",
+            "collisonCoverage": False,
+            "bodyCoverage": False,
+            "medicalCoverage": False
+        }
+    }, {
+        "vehicle": {
+            "type": "car",
+            "transmissionMethod": "auto",
+            "withDriver": True,
+            "withAc": True,
+            "noOfPassengers": 4,
+            "brandId": 6
+        },
+        "advertisement": {
+            "title": "Honda Civic for rent in Colombo",
+            "rate": 3000,
+            "rateDuration": "daily",
+            "description": "Brand new Honda Civic available for rent in Colombo.\nVery comfortable and fuel efficient.\nLow mileage and well maintained.\nDelivery available upon request.\nContact us for more details.",
+            "phone": 776543210,
+            "districtId": 5
+        },
+        "insurance": {
+            "insuranceType": "full",
+            "collisonCoverage": True,
+            "bodyCoverage": True,
+            "medicalCoverage": True
+        }
+    },
+    {
+        "vehicle": {
+            "type": "car",
+            "transmissionMethod": "manual",
+            "withDriver": False,
+            "withAc": True,
+            "noOfPassengers": 5,
+            "brandId": 13
+        },
+        "advertisement": {
+            "title": "Kia Sportage for rent in Gampaha",
+            "rate": 3500,
+            "rateDuration": "daily",
+            "description": "Luxury Kia Sportage available for rent in Gampaha.\nPowerful engine and comfortable interior.\nIdeal for long trips and family outings.\nDelivery available upon request.\nContact us for more details.",
+            "phone": 777654321,
+            "districtId": 7
+        },
+        "insurance": {
+            "insuranceType": "third",
+            "collisonCoverage": False,
+            "bodyCoverage": True,
+            "medicalCoverage": False
+        }
+    },
+    {
+        "vehicle": {
+            "type": "car",
+            "transmissionMethod": "auto",
+            "withDriver": True,
+            "withAc": True,
+            "noOfPassengers": 3,
+            "brandId": 9
+        },
+        "advertisement": {
+            "title": "Hyundai Tucson for rent in Kandy",
+            "rate": 2500,
+            "rateDuration": "daily",
+            "description": "Spacious Hyundai Tucson available for rent in Kandy.\nIdeal for business trips and airport transfers.\nDelivery available upon request.\nContact us for more details.",
+            "phone": 778765432,
+            "districtId": 11
+        },
+        "insurance": {
+            "insuranceType": "full",
+            "collisonCoverage": True,
+            "bodyCoverage": True,
+            "medicalCoverage": True
+        }
+    }, {
+        "vehicle": {
+            "type": "car",
+            "transmissionMethod": "auto",
+            "withDriver": True,
+            "withAc": True,
+            "noOfPassengers": 4,
+            "brandId": 6
+        },
+        "advertisement": {
+            "title": "Honda Civic for rent in Kaluthara",
+            "rate": 3000,
+            "rateDuration": "daily",
+            "description": "Brand new Honda Civic available for rent in Colombo.\nVery comfortable and fuel efficient.\nLow mileage and well maintained.\nDelivery available upon request.\nContact us for more details.",
+            "phone": 776543210,
+            "districtId": 5
+        },
+        "insurance": {
+            "insuranceType": "full",
+            "collisonCoverage": True,
+            "bodyCoverage": True,
+            "medicalCoverage": True
+        }
+    }, {
+        "vehicle": {
+            "type": "car",
+            "transmissionMethod": "auto",
+            "withDriver": True,
+            "withAc": True,
+            "noOfPassengers": 4,
+            "brandId": 16
+        },
+        "advertisement": {
+            "title": "Chevrolet Camaro for rent in Kandy",
+            "rate": 4000,
+            "rateDuration": "daily",
+            "description": "Experience the thrill of driving a Chevrolet Camaro in Kandy.\nPowerful engine and sleek design.\nIdeal for special occasions and weddings.\nDelivery available upon request.\nContact us for more details.",
+            "phone": 778888888,
+            "districtId": 11
+        },
+        "insurance": {
+            "insuranceType": "full",
+            "collisonCoverage": True,
+            "bodyCoverage": True,
+            "medicalCoverage": True
+        }
+    },
+    {
+        "vehicle": {
+            "type": "car",
+            "transmissionMethod": "auto",
+            "withDriver": True,
+            "withAc": True,
+            "noOfPassengers": 4,
+            "brandId": 5
+        },
+        "advertisement": {
+            "title": "Porsche 911 for rent in Colombo",
+            "rate": 8000,
+            "rateDuration": "daily",
+            "description": "Experience the ultimate luxury with a Porsche 911 in Colombo.\nPowerful engine and elegant design.\nIdeal for high-end events and business meetings.\nDelivery available upon request.\nContact us for more details.",
+            "phone": 779999999,
+            "districtId": 5
+        },
+        "insurance": {
+            "insuranceType": "full",
+            "collisonCoverage": True,
+            "bodyCoverage": True,
+            "medicalCoverage": True
+        }
+    },
+    {
+        "vehicle": {
+            "type": "car",
+            "transmissionMethod": "manual",
+            "withDriver": False,
+            "withAc": True,
+            "noOfPassengers": 5,
+            "brandId": 9
+        },
+        "advertisement": {
+            "title": "Hyundai Sonata for rent in Galle",
+            "rate": 2500,
+            "rateDuration": "daily",
+            "description": "Comfortable and affordable Hyundai Sonata available for rent in Galle.\nSpacious interior and fuel efficient.\nIdeal for daily commutes and short trips.\nDelivery available upon request.\nContact us for more details.",
+            "phone": 771111111,
+            "districtId": 6
+        },
+        "insurance": {
+            "insuranceType": "third",
+            "collisonCoverage": False,
+            "bodyCoverage": False,
+            "medicalCoverage": False
+        }
+    }
+]
