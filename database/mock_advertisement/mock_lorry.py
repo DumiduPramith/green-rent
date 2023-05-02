a2369a4aab22242303488070b71c205c75058105
DumiduPramith <dumidu42@gmail.com>
2023-05-02 22:27:57 +0530

    mock lorry data file

---
+++ database/mock_advertisement/mock_lorry.py
@@ -0,0 +1,93 @@
+LORRY = [
+    {
+        "vehicle": {
+            "type": "lorry",
+            "transmissionMethod": "auto",
+            "engineCapacity": "6000cc",
+            "withDriver": True,
+            "brandId": 1
+        },
+        "advertisement": {
+            "title": "Benz Actros 3348 for rent",
+            "rate": 1200,
+            "rateDuration": "weekly",
+            "description": "Available for long term rent.\nBrand new condition.\nContact us for more details.\n\nWe provide islandwide service.\n\n",
+            "phone": "776543210",
+            "districtId": 5
+        },
+        "insurance": {
+            "insuranceType": "full",
+            "collisonCoverage": True,
+            "bodyCoverage": True,
+            "medicalCoverage": True
+        }
+    },
+    {
+        "vehicle": {
+            "type": "lorry",
+            "transmissionMethod": "manual",
+            "engineCapacity": "3500cc",
+            "withDriver": False,
+            "brandId": 4
+        },
+        "advertisement": {
+            "title": "Hino Dutro for sale",
+            "rate": 3000000,
+            "rateDuration": "monthly",
+            "description": "Good running condition.\nNewly painted.\n\nCall for inspection and negotiation.\n\n",
+            "phone": "777777777",
+            "districtId": 11
+        },
+        "insurance": {
+            "insuranceType": "third",
+            "collisonCoverage": False,
+            "bodyCoverage": True,
+            "medicalCoverage": False
+        }
+    }, {
+        "vehicle": {
+            "type": "lorry",
+            "transmissionMethod": "auto",
+            "engineCapacity": "5000cc",
+            "withDriver": True,
+            "brandId": 1
+        },
+        "advertisement": {
+            "title": "Benz Actros 2535L for Rent",
+            "rate": 1500,
+            "rateDuration": "daily",
+            "description": "Benz Actros 2535L lorry with auto transmission and 5000cc engine capacity available for rent.\n\nIdeal for heavy duty hauling and transportation.\n\nWith driver provided.\n\nContact us for more details.",
+            "phone": "712345678",
+            "districtId": 5
+        },
+        "insurance": {
+            "insuranceType": "third",
+            "collisonCoverage": False,
+            "bodyCoverage": True,
+            "medicalCoverage": True
+        }
+    },
+    {
+        "vehicle": {
+            "type": "lorry",
+            "transmissionMethod": "manual",
+            "engineCapacity": "8000cc",
+            "withDriver": False,
+            "brandId": 3
+        },
+        "advertisement": {
+            "title": "Volvo FMX 440 for Sale",
+            "rate": 3500000,
+            "rateDuration": "monthly",
+            "description": "Volvo FMX 440 lorry with manual transmission and 8000cc engine capacity available for sale.\n\nIn excellent condition, with low mileage and regular maintenance.\n\nIdeal for heavy duty hauling and transportation.\n\nContact us for more details.",
+            "phone": "778945612",
+            "districtId": 11
+        },
+        "insurance": {
+            "insuranceType": "full",
+            "collisonCoverage": True,
+            "bodyCoverage": True,
+            "medicalCoverage": True
+        }
+    }
+]
